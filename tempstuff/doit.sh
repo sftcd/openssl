3f51af0891b36b9fafc272ecf41e1dbda65bb573
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2018-11-10 16:32:24 +0000

    valgrind looking ok now

--- tempstuff/doit.sh
+++ tempstuff/doit.sh
@@ -4,5 +4,11 @@ export LD_LIBRARY_PATH=$HOME/code/openssl
 ESNI="/wHHBBOoACQAHQAg4YSfjSyJPNr1z3F8KqzBNBnMejim0mJZaPmria3XsicAAhMBAQQAAAAAW9pQEAAAAABb4jkQAAA="
 COVER="cloudflare.net"
 HIDDEN="encryptedsni.com"
+VG="no"
 
-./esni $HIDDEN $COVER $ESNI
+if [[ "$VG" == "mo" ]]
+then
+	./esni $HIDDEN $COVER $ESNI
+else
+	valgrind --leak-check=full ./esni $HIDDEN $COVER $ESNI
+fi
