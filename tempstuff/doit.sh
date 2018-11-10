1b3a872b337c9a0adcc2c3e98c6216fd3a0e62a6
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2018-11-10 16:52:10 +0000

    allow doit.sh to download new value via dig

--- tempstuff/doit.sh
+++ tempstuff/doit.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$HOME/code/openssl
+#set -x
+
+# to pick up correct .so's - maybe note 
+#export LD_LIBRARY_PATH=$HOME/code/openssl
+
+# this is one I downloaded manually via dig +short TXT _esni.encryptedsni.com
 ESNI="/wHHBBOoACQAHQAg4YSfjSyJPNr1z3F8KqzBNBnMejim0mJZaPmria3XsicAAhMBAQQAAAAAW9pQEAAAAABb4jkQAAA="
 COVER="cloudflare.net"
 HIDDEN="encryptedsni.com"
 VG="no"
+
+# should really add getopt but this is likely short-term (famous last
+# words those:-)
+
+if [[ "$1" == "fresh" ]]
+then
+	echo "Checking for fresh ESNI value from $HIDDEN"
+	ESNI=`dig +short TXT _esni.$HIDDEN | sed -e 's/"//g'`	
+	echo "Fresh ESNI value: $ESNI"
+fi	
 
 if [[ "$VG" == "no" ]]
 then
