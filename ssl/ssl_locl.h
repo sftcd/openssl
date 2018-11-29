b3f6b08da62f3d31f0c1a9cb430214bb0e1e5b4a
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2018-11-29 16:29:44 +0000

    testit fixes, consistency++ for some names

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -1283,7 +1283,8 @@ struct ssl_st {
         void *debug_arg;
         char *hostname;
 #ifndef OPENSSL_NO_ESNI
-		char *enchostname;
+		char *encservername;
+		char *covername;
 #endif
         /* certificate status request info */
         /* Status type or -1 if no status type */
--- ssl/statem/extensions_clnt.c
+++ ssl/statem/extensions_clnt.c
@@ -40,6 +40,41 @@ EXT_RETURN tls_construct_ctos_server_name(SSL *s, WPACKET *pkt,
     if (s->ext.hostname == NULL)
         return EXT_RETURN_NOT_SENT;
 
+#ifndef OPENSSL_NO_ESNI
+	if (s->esni != NULL ) {
+		/*
+		 * Check that s.ext.hostname == s.ext.esni.covername
+		 * and s.ext.covername != s.ext.encserername (which
+		 * shouldn't happen ever but who knows...)
+		 * If either test fails don't send server_naeme. 
+		 * That is, if we want to send ESNI, then we only
+		 * send SNI if the covername was explicitly set 
+		 * and is the same as the SNI (that maybe got set
+		 * via some weirdo application API that we couldn't
+		 * change when ESNI enabling perhaps)
+		 */
+    	if (s->ext.encservername == NULL) { /* ouch, also shouldn't happen */
+        	SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_F_TLS_CONSTRUCT_CTOS_SERVER_NAME,
+                 ERR_R_INTERNAL_ERROR);
+        	return EXT_RETURN_FAIL;
+		}
+		if (s->ext.covername==NULL) {
+        	return EXT_RETURN_NOT_SENT;
+		}
+		size_t fn_len=OPENSSL_strnlen(s->ext.covername,TLSEXT_MAXLEN_host_name);
+		size_t hn_len=OPENSSL_strnlen(s->ext.hostname,TLSEXT_MAXLEN_host_name);
+		size_t en_len=OPENSSL_strnlen(s->ext.encservername,TLSEXT_MAXLEN_host_name);
+		if (fn_len!=hn_len || !CRYPTO_memcmp(s->ext.hostname,s->ext.covername,fn_len)) {
+        	return EXT_RETURN_NOT_SENT;
+		}
+		if (en_len!=fn_len || !CRYPTO_memcmp(s->ext.encservername,s->ext.covername,fn_len)) {
+        	SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_F_TLS_CONSTRUCT_CTOS_SERVER_NAME,
+                 ERR_R_INTERNAL_ERROR);
+        	return EXT_RETURN_FAIL;
+		}
+	}
+#endif
+
     /* Add TLS extension servername to the Client Hello message */
     if (!WPACKET_put_bytes_u16(pkt, TLSEXT_TYPE_server_name)
                /* Sub-packet for server_name extension */
@@ -1998,7 +2033,7 @@ EXT_RETURN tls_construct_ctos_esni(SSL *s, WPACKET *pkt, unsigned int context,
         return EXT_RETURN_NOT_SENT;
     }
 
-    if (s->ext.enchostname == NULL) {
+    if (s->ext.encservername == NULL) {
         return EXT_RETURN_NOT_SENT;
     }
     
@@ -2028,7 +2063,7 @@ EXT_RETURN tls_construct_ctos_esni(SSL *s, WPACKET *pkt, unsigned int context,
 
     uint16_t curve_id = s->s3->group_id;
     CLIENT_ESNI *c=NULL;
-    if (!SSL_ESNI_enc(s->esni,s->ext.enchostname,s->ext.hostname,rd_len,rd,curve_id,ks_len,ks,&c)) {
+    if (!SSL_ESNI_enc(s->esni,s->ext.encservername,s->ext.covername,rd_len,rd,curve_id,ks_len,ks,&c)) {
         return 0;
     }
     OPENSSL_free(ks);
