fd51fed9cd73c869110de48195edbca7acb3cef5
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2021-08-23 23:27:27 +0100

    one more try to get windows stuff right

--- apps/s_server.c
+++ apps/s_server.c
@@ -602,9 +602,6 @@ static int ssl_ech_servername_cb(SSL *s, int *ad, void *arg)
     char lstr[ECH_TIME_STR_LEN];
 #if !defined(OPENSSL_SYS_WINDOWS)
     local_p=gmtime_r(&now,&local);
-#else
-    local_p=gmtime_s(&local,&now);
-#endif
     if (local_p!=&local) {
         strcpy(lstr,"sometime");
     } 
