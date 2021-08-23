f2c41a819de25dc023adfe5f81a034a1da3232d1
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2021-08-23 22:02:55 +0100

    re-fix windows gmtime_s

--- apps/s_server.c
+++ apps/s_server.c
@@ -602,6 +602,9 @@ static int ssl_ech_servername_cb(SSL *s, int *ad, void *arg)
     char lstr[ECH_TIME_STR_LEN];
 #if !defined(OPENSSL_SYS_WINDOWS)
     local_p=gmtime_r(&now,&local);
+#else
+    local_p=gmtime_r(&local,&now);
+#endif
     if (local_p!=&local) {
         strcpy(lstr,"sometime");
     } 
