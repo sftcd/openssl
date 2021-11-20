50fc514db66148b31a9ea35a41fa2723914fd9ce
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2021-11-20 00:18:09 +0000

    better clean up after UB san fix (2nd time;-)

--- test/sslapitest.c
+++ test/sslapitest.c
@@ -9644,9 +9644,6 @@ static int test_ech_add(int idx)
     int echcount = 0;
     int returned;
 
-    /* No ECH for FIPS for now */
-    if (is_fips) return 1;
-
 #if 0
     /*
      * This ECHConfigList has only one entry.
