01a0a2bfb1c338f67e4496b8437c4f59ea153ffa
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2022-02-17 14:07:03 +0000

    tidier/fuller NSS client interop test script (not done though)

--- esnistuff/nssdoech.sh
+++ esnistuff/nssdoech.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-#  set -x
+# set -x
+
+# 2022-02-17 - got basic interop for my NSS build with CF and defo
+# services
+# - something up with port 8414 (forced HRR), not sure what's what yet
+# - need to re-do the localhost stuff still (didn't try that at all and 
+#   the cadir I have from a year ago has a now-outdated format). Some of
+# - the HTTP respsonse content that this gets back is confusing and 
+#   should be updated
 
 LDIR=/home/stephen/code/dist/Debug/
 RDIR=/home/stephen/code/openssl/esnistuff
@@ -11,70 +19,91 @@ export LD_LIBRARY_PATH=$LDIR/lib
 #export SSLTRACE=100
 #export SSLDEBUG=100
 
+function b64_ech_from_DNS()
+{
+    host=$1
+    port=$2
+    if [[ "$port" == "" ]]
+    then
+        port=443
+        qname="$host"
+    elif [[ "$port" == "443" ]]
+    then
+        qname="$host"
+    else
+        qname="_$port._https.$host"
+    fi
+    ECHRR=`dig +short -t TYPE65 $qname | \
+        tail -1 | cut -f 3- -d' ' | sed -e 's/ //g' | sed -e 'N;s/\n//'`
+    if [[ "$ECHRR" == "" ]]
+    then
+        echo "Can't read ECHConfigList for $host:$port"
+        exit 2
+    fi
+    # extract ECHConfigs from RR
+    marker="FE0D"
+    prefix=${ECHRR%%$marker*}
+    index=${#prefix}
+    ec_ah_ind=$((index-4))
+    e_ah_len=${ECHRR:ec_ah_ind:4}
+    ech_len=$(((2*(16#$e_ah_len+4))-3))
+    ech_str=${ECHRR:ec_ah_ind:ech_len}
+    ECH=`echo $ech_str | xxd -r -p | base64 -w0`
+    echo $ECH
+}
+
+
 if [ ! -f $LDIR/bin/tstclnt ]
 then
 	echo "You need an NSS build first - can't find  $LDIR/bin/tstclnt"
 	exit 1
 fi
 
-NSSPARAMS=" -D -b "
-
-# 2022-02-17 - got basic interop for my NSS build with CF and defo:8413
-# need to tidy this up a lot and add other defo.ie ports and re-do the
-# localhost tests (didn't try that at all and the cadir I have from a
-# year ago has a now-outdated format).
+# the -4 seems to be down to some f/w oddity causing IPv6
+# connections to fail from one vantage point - we don't need
+# to care though, so we can just do IPv4 for now
+NSSPARAMS=" -4 -D -b "
 
 if [[ "$1" == "localhost" ]]
 then
-	ECHCFG=`cat echconfig-256.pem | tail -2 | head -1`
-	echo "Running: valgrind $LDIR/bin/tstclnt $NSSPARAMS -h localhost -p 8443 -a foo.example.com -d cadir/nssca/ -N $ECHCFG" 
-	valgrind $LDIR/bin/tstclnt $NSSPARAMS -h localhost -p 8443 -a foo.example.com -d cadir/nssca/ -N $ECHCFG
-elif [[ "$1" == "defo" ]]
-then
-    defohost="draft-13.esni.defo.ie"
-    defoport="8413"
-    defohttpreq="GET /stats HTTP/1.1\\r\\nConnection: close\\r\\nHost: $defohost\\r\\n\\r\\n"
-    ECHRR=`dig +short -t TYPE65 "_$defoport._https.$defohost" | \
-        tail -1 | cut -f 3- -d' ' | sed -e 's/ //g' | sed -e 'N;s/\n//'`
-    if [[ "$ECHRR" == "" ]]
-    then
-        echo "Can't read ECHConfigList for $defohost:$defoport"
-        exit 2
-    fi
-    # extract ECHConfigs from RR - TODO: make this a function!
-    marker="FE0D"
-    prefix=${ECHRR%%$marker*}
-    index=${#prefix}
-    ec_ah_ind=$((index-4))
-    e_ah_len=${ECHRR:ec_ah_ind:4}
-    ech_len=$(((2*(16#$e_ah_len+4))-3))
-    ech_str=${ECHRR:ec_ah_ind:ech_len}
-    ECH=`echo $ech_str | xxd -r -p | base64 -w0`
-
-    # this does get content and exit but leaves a TIME_WAIT socket - that's ok for now
-    echo -e $defohttpreq | timeout 1s $LDIR/bin/tstclnt $NSSPARAMS -h $defohost -p $defoport -D -N $ECH 
-else
-    # CF URL that works for FF: https://crypto.cloudflare.com/cdn-cgi/trace
-    CFFE="crypto.cloudflare.com"
-    #TARGET="encryptedsni.com"
-    TARGET="rte.ie"
-    httpreq="GET /cdn-cgi/trace HTTP/1.1\\r\\nConnection: close\\r\\nHost: $TARGET\\r\\n\\r\\n"
-
-    ECHRR=`dig +short -t TYPE65 $CFFE | tail -1 | cut -f 3- -d' ' | sed -e 's/ //g' | sed -e 'N;s/\n//'`
-    if [[ "$ECHRR" == "" ]]
-    then
-        echo "Can't read ECHConfigList for $CFFE"
-        exit 2
-    fi
-    # extract ECHConfigs from RR - TODO: make this a function!
-    marker="FE0D"
-    prefix=${ECHRR%%$marker*}
-    index=${#prefix}
-    ec_ah_ind=$((index-4))
-    e_ah_len=${ECHRR:ec_ah_ind:4}
-    ech_len=$(((2*(16#$e_ah_len+4))-3))
-    ech_str=${ECHRR:ec_ah_ind:ech_len}
-    ECH=`echo $ech_str | xxd -r -p | base64 -w0`
-
-    echo -e $httpreq | timeout 1s $LDIR/bin/tstclnt $NSSPARAMS -h $CFFE -a $TARGET -p 443 -N $ECH $*
+	ECH=`cat d13.pem | tail -2 | head -1`
+	echo "Running: $LDIR/bin/tstclnt -b -h localhost -p 8443 -a foo.example.com -d cadir/nssca/ -N $ECH $*"
+	$LDIR/bin/tstclnt -b -h localhost -p 8443 -a foo.example.com -d cadir/nssca/ -N $ECH $*
+    exit $?
 fi
+
+# service specific details as CSVs...
+cfdets="crypto.cloudflare.com,encryptedsni.com,443,cdn-cgi/trace"
+cfrte="crypto.cloudflare.com,rte.ie,443,cdn-cgi/trace"
+defo8413="draft-13.esni.defo.ie,draft-13.esni.defo.ie,8413,stats"
+defo8414="draft-13.esni.defo.ie,draft-13.esni.defo.ie,8414,stats"
+defo9413="draft-13.esni.defo.ie,draft-13.esni.defo.ie,9413," 
+defo10413="draft-13.esni.defo.ie,draft-13.esni.defo.ie,10413," 
+defo11413="draft-13.esni.defo.ie,draft-13.esni.defo.ie,11413," 
+defo12413="draft-13.esni.defo.ie,draft-13.esni.defo.ie,12413," 
+defo12414="draft-13.esni.defo.ie,draft-13.esni.defo.ie,12414," 
+
+services="$cfdets $cfrte \
+    $defo8413 $defo8414 \
+    $defo9413 \
+    $defo10413 $defo11413 \
+    $defo12413 $defo12414"
+items=${#services[@]}
+
+for item in $services
+do
+    echo "Doing $item"
+    host=`echo $item | awk -F, '{print $1}'`
+    innerhost=`echo $item | awk -F, '{print $2}'`
+    port=`echo $item | awk -F, '{print $3}'`
+    path=`echo $item | awk -F, '{print $4}'`
+    httpreq="GET /$path HTTP/1.1\\r\\nConnection: close\\r\\nHost: $innerhost\\r\\n\\r\\n"
+    ECH=`b64_ech_from_DNS $host $port`
+    echo "Running: echo -e $httpreq | $LDIR/bin/tstclnt $NSSPARAMS -h $host -p $port -a $innerhost -N $ECH "
+    echo -e $httpreq | timeout 1s $LDIR/bin/tstclnt $NSSPARAMS -h $host -p $port -a $innerhost -N $ECH 
+    res=$?
+    echo "res is: $res"
+    echo "-----------------------" 
+    echo "-----------------------" 
+done
+
