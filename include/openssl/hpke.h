6e539323eb8c79b93f84a527aa0325964e0ea480
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2022-09-06 00:52:58 +0100

    end of night

--- include/openssl/hpke.h
+++ include/openssl/hpke.h
@@ -149,6 +149,7 @@ extern "C" {
 # define OSSL_HPKE_AEADSTR_AES128GCM  LN_aes_128_gcm       /**< AEAD id 1 */
 # define OSSL_HPKE_AEADSTR_AES256GCM  LN_aes_256_gcm       /**< AEAD id 2 */
 # define OSSL_HPKE_AEADSTR_CP         LN_chacha20_poly1305 /**< AEAD id 3 */
+# define OSSL_HPKE_AEADSTR_EXP        "exporter"           /**< AEAD id 0xff */
 
 /**
  * @brief ciphersuite combination
@@ -307,26 +308,14 @@ int OSSL_HPKE_CTX_set1_authpub(OSSL_HPKE_CTX *ctx,
                                size_t publen);
 
 /**
- * @brief set an exporter length and context for HPKE
- * @param ctx is the pointer for the HPKE context
- * @param exp_ctx is the exporter context octets
- * @param exp_ctxlen is the size of exp_ctx
- * @param explen is the desired exporter output size
- * @return 1 for success, 0 for error
- */
-int OSSL_HPKE_CTX_set1_exporter(OSSL_HPKE_CTX *ctx,
-                                const unsigned char *exp_ctx, size_t exp_ctxlen,
-                                size_t explen);
-
-/**
  * @brief ask for the state of the sequence of seal/open calls
  * @param ctx is the pointer for the HPKE context
- * @return seq returns the positive integer sequence number
+ * @param seq returns the positive integer sequence number
  * @return 1 for success, 0 for error
  *
  * The value returned is the next one to be used when sealing
- * or opening (so if we start at zero this will be 1 after the
- * first successful call to seal)
+ * or opening (so as we start at zero this will be 1 after the
+ * first successful call to seal or open)
  *
  * seq is a uint64_t as that's what two other implementations
  * chose
@@ -350,8 +339,6 @@ int OSSL_HPKE_CTX_set1_seq(OSSL_HPKE_CTX *ctx, uint64_t seq);
  * @param enclen is the size the above
  * @param ct is the ciphertext output
  * @param ctlen is the size the above
- * @param exp is the exporter octets
- * @param explen is the size the above
  * @param pub is the recipient public key octets
  * @param publen is the size the above
  * @param recip is the EVP_PKEY form of recipient public value
@@ -377,7 +364,6 @@ int OSSL_HPKE_CTX_set1_seq(OSSL_HPKE_CTX *ctx, uint64_t seq);
 int OSSL_HPKE_sender_seal(OSSL_HPKE_CTX *ctx,
                           unsigned char *enc, size_t *enclen,
                           unsigned char *ct, size_t *ctlen,
-                          unsigned char *exp, size_t *explen,
                           unsigned char *pub, size_t publen,
                           const unsigned char *info, size_t infolen,
                           const unsigned char *aad, size_t aadlen,
@@ -390,8 +376,6 @@ int OSSL_HPKE_sender_seal(OSSL_HPKE_CTX *ctx,
  * @param ptlen is the size the above
  * @param enc is the sender's ephemeral public value
  * @param enclen is the size the above
- * @param exp is the exporter octets
- * @param explen is the size the above
  * @param recippriv is the EVP_PKEY form of recipient private value
  * @param info is the info parameter
  * @param infolen is the size the above
@@ -414,52 +398,29 @@ int OSSL_HPKE_sender_seal(OSSL_HPKE_CTX *ctx,
 int OSSL_HPKE_recipient_open(OSSL_HPKE_CTX *ctx,
                              unsigned char *pt, size_t *ptlen,
                              const unsigned char *enc, size_t enclen,
-                             unsigned char *exp, size_t *explen,
                              EVP_PKEY *recippriv,
                              const unsigned char *info, size_t infolen,
                              const unsigned char *aad, size_t aadlen,
                              const unsigned char *ct, size_t ctlen);
 
 /**
- * @brief sender export-only function
- * @param ctx is the pointer for the HPKE context
- * @param enc is the sender's ephemeral public value
- * @param enclen is the size the above
- * @param exp is the exporter octets
- * @param explen is the size the above
- * @param pub is the recipient public key octets
- * @param publen is the size the above
- * @param info is the key schedule info parameter
- * @param infolen is the size the above
- * @return 1 for success, 0 for error
+ * @brief generate a given-length secret based on context and label
+ * @param ctx is the HPKE context
+ * @param secret is the resulting secret that will be of length...
+ * @param secret_len is the desired output length
+ * @param label is a buffer to provide separation between secrets
+ * @param labellen is the length of the above
+ * @return 1 for good, 0 for error
  *
- * This can be called once, or multiple, times.
+ * The context has to have been used already for one encryption
+ * or decryption for this to work (as this is based on the negotiated
+ * "exporter_secret" estabilshed via the HPKE operation).
  */
-int OSSL_HPKE_export_only_sender(OSSL_HPKE_CTX *ctx,
-                                 unsigned char *enc, size_t *enclen,
-                                 unsigned char *exp, size_t *explen,
-                                 unsigned char *pub, size_t publen,
-                                 const unsigned char *info, size_t infolen);
-
-/**
- * @brief receiver export-only function
- * @param ctx is the pointer for the HPKE context
- * @param enc is the sender's ephemeral public value
- * @param enclen is the size the above
- * @param exp is the exporter octets
- * @param explen is the size the above
- * @param recippriv is the EVP_PKEY form of recipient private value
- * @param info is the key schedule info parameter
- * @param infolen is the size the above
- * @return 1 for success, 0 for error
- *
- * This can be called once, or multiple, times.
- */
-int OSSL_HPKE_export_only_recip(OSSL_HPKE_CTX *ctx,
-                                unsigned char *enc, size_t enclen,
-                                unsigned char *exp, size_t *explen,
-                                EVP_PKEY *recippriv,
-                                const unsigned char *info, size_t infolen);
+int OSSL_HPKE_CTX_export(OSSL_HPKE_CTX *ctx,
+                         unsigned char *secret,
+                         size_t secret_len,
+                         const unsigned char *label,
+                         size_t labellen);
 
 /**
  * @brief generate a key pair
@@ -604,60 +565,6 @@ int OSSL_HPKE_enc(OSSL_LIB_CTX *libctx, const char *propq,
                   unsigned char *senderpub, size_t *senderpublen,
                   EVP_PKEY *senderpriv,
                   unsigned char *cipher, size_t *cipherlen);
-
-/**
- * @brief HPKE multi-shot encryption function
- *
- * This function generates a non-ephemeral ECDH value internally and
- * provides the public and private components as outputs. The public
- * part can be sent to the relevant private key holder along with the
- * ciphertext. The private part can be re-used in subequent calls.
- *
- * Note that the sender's public value is an input here (as is the
- * sender's private value), in contrast to the case of OSSL_HPKE_enc
- * where the sender's public value is an output.
- *
- * @param libctx is the context to use
- * @param propq is a properties string
- * @param mode is the HPKE mode
- * @param suite is the ciphersuite to use
- * @param pskid is the pskid string for a PSK mode (can be NULL)
- * @param psk is the psk
- * @param psklen is the psk length
- * @param pub is the encoded public key
- * @param publen is the length of the public key
- * @param authpriv is the encoded private (authentication) key
- * @param authprivlen is the length of the private (authentication) key
- * @param authpriv_evp is the EVP_PKEY* form of private (authentication) key
- * @param clear is the encoded cleartext
- * @param clearlen is the length of the cleartext
- * @param aad is the encoded additional data
- * @param aadlen is the length of the additional data
- * @param info is the encoded info data (can be NULL)
- * @param infolen is the length of the info data (can be zero)
- * @param seq is the encoded sequence data (can be NULL)
- * @param seqlen is the length of the sequence data (can be zero)
- * @param senderpub is the input buffer for sender public key
- * @param senderpublen length of the input buffer for sender's public key
- * @param senderpriv is the EVP_PKEY* form of sender key pair
- * @param cipher is the input buffer for ciphertext
- * @param cipherlen is the length of the input buffer for ciphertext
- * @return 1 for success, other for error (error returns can be non-zero)
- */
-int OSSL_HPKE_enc_evp(OSSL_LIB_CTX *libctx, const char *propq,
-                      unsigned int mode, OSSL_HPKE_SUITE suite,
-                      const char *pskid,
-                      const unsigned char *psk, size_t psklen,
-                      const unsigned char *pub, size_t publen,
-                      const unsigned char *authpriv, size_t authprivlen,
-                      EVP_PKEY *authpriv_evp,
-                      const unsigned char *clear, size_t clearlen,
-                      const unsigned char *aad, size_t aadlen,
-                      const unsigned char *info, size_t infolen,
-                      const unsigned char *seq, size_t seqlen,
-                      const unsigned char *senderpub, size_t senderpublen,
-                      EVP_PKEY *senderpriv,
-                      unsigned char *cipher, size_t *cipherlen);
 
 /**
  * @brief HPKE single-shot decryption function
