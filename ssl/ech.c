1f5716a763112376b15201aa71dc8c0bada1e113
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2022-09-23 00:59:10 +0100

    seems to work for ECH

--- ssl/ech.c
+++ ssl/ech.c
@@ -3932,18 +3932,12 @@ int ech_swaperoo(SSL_CONNECTION *s)
     BIO_up_ref(s->wbio);
     s->rbio=tmp_outer.rbio;
     BIO_up_ref(s->rbio);
-    /* This one doesn't seem to be needed nor up-ref'd */
-    /* but not fully sure.... */
-    /* s->bbio=tmp_outer.bbio; */
 
-    /* Fields we (for now) need the same in both */
-    s->rlayer=tmp_outer.rlayer;
-    s->rlayer.s=s;
     s->init_buf=tmp_outer.init_buf;
     s->init_msg=tmp_outer.init_msg;
     s->init_off=tmp_outer.init_off;
     s->init_num=tmp_outer.init_num;
-    
+
     /* HRR processing */
     s->hello_retry_request=tmp_outer.hello_retry_request;
 
