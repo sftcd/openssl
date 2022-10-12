483ba0b0b0cdd581d7f668a23c8d951a5f56ab23
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2022-10-12 23:10:57 +0100

    fixed leak

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -1479,9 +1479,11 @@ void ossl_ssl_connection_free(SSL *ssl)
     /* Ignore return value */
     RECORD_LAYER_clear(&s->rlayer);
 
+#if 0
 #ifndef OPENSSL_NO_ECH
     /* This seems needed on client but not on server */
     if ( (!s->server && (s->ext.inner_s || !s->ech)) || s->server)
+#endif
 #endif
     BUF_MEM_free(s->init_buf);
 
@@ -1593,12 +1595,16 @@ void ossl_ssl_connection_free(SSL *ssl)
 
     /* Free up the inner or outer, as needed */
     if (s->ext.outer_s!=NULL && s->ext.outer_s!=s)  {
+        if (s->init_buf == s->ext.outer_s->init_buf)
+            s->ext.outer_s->init_buf = NULL;
         /* Don't go around in circles forever */
         s->ext.outer_s->ext.inner_s=NULL;
         SSL_free(&s->ext.outer_s->ssl);
         s->ext.outer_s=NULL;
     }
     if (s->ext.inner_s!=NULL && s->ext.inner_s!=s)  {
+        if (s->init_buf == s->ext.inner_s->init_buf)
+            s->ext.inner_s->init_buf = NULL;
         /* Don't go around in circles forever */
         s->ext.inner_s->ext.outer_s=NULL;
         SSL_free(&s->ext.inner_s->ssl);
