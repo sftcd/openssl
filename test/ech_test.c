f6d77b73724047d9886bdef590870f96caac0c0f
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2022-11-06 00:55:25 +0000

    bit moar ech_test.c stuff, with an application driven leak to fix!

--- test/ech_test.c
+++ test/ech_test.c
@@ -7,12 +7,14 @@
  * https://www.openssl.org/source/license.html
  */
 
+#include <openssl/ssl.h>
 #include <openssl/ech.h>
 #include <openssl/hpke.h>
 #include "testutil.h"
 
-//static OSSL_LIB_CTX *testctx = NULL;
-//static char *testpropq = NULL;
+static OSSL_LIB_CTX *testctx = NULL;
+static char *testpropq = NULL;
+static BIO *bio_stdout = NULL;
 
 static int basic_echconfig_gen(void)
 {
@@ -27,18 +29,48 @@ static int basic_echconfig_gen(void)
     OSSL_HPKE_SUITE hpke_suite = OSSL_HPKE_SUITE_DEFAULT;
     unsigned char *extvals = NULL;
     size_t extlen = 0;
+    SSL_CTX *ctx  = NULL;
+    SSL *ssl = NULL;
+    int num_echs = 0;
+    OSSL_ECH_DETS *details = NULL;
+    int num_dets = 0;
 
     res=ossl_ech_make_echconfig(echconfig, &echconfig_len, priv, &privlen,
                                      ech_version, max_name_length, public_name,
                                      hpke_suite, extvals, extlen);
     if (!TEST_int_eq(res,1))
         return 0;
+    if (!TEST_ptr(ctx = SSL_CTX_new_ex(testctx, testpropq,
+                                       TLS_server_method())))
+        return 0;
+    if (!TEST_ptr(ssl = SSL_new(ctx)))
+        return 0;
+    res = SSL_ech_add(ssl, OSSL_ECH_FMT_GUESS, echconfig_len, (char *)echconfig,
+                      &num_echs);
+    if (!TEST_int_eq(res,1))
+        return 0;
+    /* add same one a 2nd time for fun */
+    /* TODO: fix leak!!! */
+    res = SSL_ech_add(ssl, OSSL_ECH_FMT_GUESS, echconfig_len, (char *)echconfig,
+                      &num_echs);
+    if (!TEST_int_eq(res,1))
+        return 0;
+    res = SSL_ech_query(ssl, &details, &num_dets);
+    if (!TEST_int_eq(res,1))
+        return 0;
+    res = OSSL_ECH_DETS_print(bio_stdout, details, num_dets);
+    if (!TEST_int_eq(res,1))
+        return 0;
 
-    return res;
+    OSSL_ECH_DETS_free(details, num_dets);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    return 1;
 }
 
 int setup_tests(void)
 {
+    bio_stdout = BIO_new_fp(stdout, BIO_NOCLOSE | BIO_FP_TEXT);
     ADD_TEST(basic_echconfig_gen);
     return 1;
 }
