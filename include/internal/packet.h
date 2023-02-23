1660d96e98e0aa55b21e2ae6218fc3a3857c58c4
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2023-02-23 02:18:06 +0000

    rebase-build

--- include/internal/packet.h
+++ include/internal/packet.h
@@ -680,29 +680,6 @@ __owur static ossl_inline int PACKET_get_quic_length_prefixed(PACKET *pkt,
 }
 # endif
 
-# ifndef OPENSSL_NO_ECH
-/*
- * New ECH API allowing replacement of outer client hello on server
- * with inner.
- */
-__owur static ossl_inline int PACKET_replace(PACKET *pkt, PACKET *newpkt)
-{
-    const unsigned char *data;
-
-    if (pkt == NULL || newpkt == NULL)
-        return 0;
-    if (newpkt->remaining > pkt->remaining) {
-        data = OPENSSL_realloc((unsigned char *)pkt->curr, newpkt->remaining);
-        if (data == NULL)
-            return 0;
-        pkt->curr = data;
-    }
-    memcpy((unsigned char *)pkt->curr, newpkt->curr, newpkt->remaining);
-    pkt->remaining = newpkt -> remaining;
-    return 1;
-}
-# endif
-
 /* Writeable packets */
 
 typedef struct wpacket_sub WPACKET_SUB;
