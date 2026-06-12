eeab16a2b003926fe04b809b55f49969ad4b12ef
sftcd <stephen.farrell@cs.tcd.ie>
2026-06-12 01:53:43 +0100

    fixup! fix GREASE extension type handing with ECH and adds a test for that

--- ssl/statem/extensions_clnt.c
+++ ssl/statem/extensions_clnt.c
@@ -2876,11 +2876,11 @@ EXT_RETURN tls_construct_ctos_grease1(SSL_CONNECTION *s, WPACKET *pkt,
 {
     uint16_t grease_type;
  
+    if (!(s->options & SSL_OP_GREASE) || s->server)
+        return EXT_RETURN_NOT_SENT;
 #ifndef OPENSSL_NO_ECH
     ECH_SAME_EXT(s, context, pkt)
 #endif
-    if (!(s->options & SSL_OP_GREASE) || s->server)
-        return EXT_RETURN_NOT_SENT;
 
     grease_type = ossl_grease_value(s, OSSL_GREASE_EXT1);
 
@@ -2899,11 +2899,11 @@ EXT_RETURN tls_construct_ctos_grease2(SSL_CONNECTION *s, WPACKET *pkt,
 {
     uint16_t grease_type;
 
+    if (!(s->options & SSL_OP_GREASE) || s->server)
+        return EXT_RETURN_NOT_SENT;
 #ifndef OPENSSL_NO_ECH
     ECH_SAME_EXT(s, context, pkt)
 #endif
-    if (!(s->options & SSL_OP_GREASE) || s->server)
-        return EXT_RETURN_NOT_SENT;
 
     grease_type = ossl_grease_value(s, OSSL_GREASE_EXT2);
 
